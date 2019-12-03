0d4cb57d47616f825adf494560248cc79dac0f82
tachawin <44483385+tachawin@users.noreply.github.com>
2019-12-03 16:06:48 +0700

    Delete config.js

--- app/config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    'secret': 'IssaPongTigerPound',
-    accessKeyId: 'AKIAWMSMPJL7YQQ327WX',
-    secretAccessKey: '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',
-    Bucket: 'pongtest5555'
-  };
