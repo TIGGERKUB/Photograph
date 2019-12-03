a5199bd9e9efb0a83b938a8ddc0cfefc139fb490
TACHAWIN WIWATTANASOPA <tachawin.ptw@mail.kmutt.ac.th>
2019-12-03 16:05:31 +0700

    hide secret

--- app/config/config.js
+++ app/config/config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    'secret': 'IssaPongTigerPound'
+    'secret': 'IssaPongTigerPound',
+    accessKeyId: 'AKIAWMSMPJL7YQQ327WX',
+    secretAccessKey: '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',
+    Bucket: 'pongtest5555'
   };
